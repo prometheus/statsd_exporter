afd1adc5b6bfd7b4c91213c1bcf3e0fd8c6b327b
jwfang <54740235@qq.com>
2017-07-12 09:37:15 +0800

    statsd.listen-tcp default to true

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ var (
 	metricsEndpoint     = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
 	statsdListenAddress = flag.String("statsd.listen-address", ":9125", "The UDP/TCP address on which to receive statsd metric lines.")
 	statsdListenUDP     = flag.Bool("statsd.listen-udp", true, "Whether to receive UDP statsd metrics.")
-	statsdListenTCP     = flag.Bool("statsd.listen-tcp", false, "Whether to receive TCP statsd metrics.")
+	statsdListenTCP     = flag.Bool("statsd.listen-tcp", true, "Whether to receive TCP statsd metrics.")
 	mappingConfig       = flag.String("statsd.mapping-config", "", "Metric mapping configuration file name.")
 	readBuffer          = flag.Int("statsd.read-buffer", 0, "Size (in bytes) of the operating system's transmit read buffer associated with the UDP connection. Please make sure the kernel parameters net.core.rmem_max is set to a value greater than the value specified.")
 	addSuffix           = flag.Bool("statsd.add-suffix", true, "Add the metric type (counter/gauge/timer) as suffix to the generated Prometheus metric (NOT recommended, but set by default for backward compatibility).")
