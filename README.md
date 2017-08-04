892b725fafce5e48d074faeb63ce2a5780d02c76
Dave Rawks <dave@pandora.com>
2017-08-04 15:41:02 -0700

    Removes support for legacy mapper config
      * Removed legacy mapper config parser
      * Removed logic to switch to YAML parsing based on mapping filename
      * Removed tests for legacy mapper config
      * Remove references to legacy mapper config format from `README.md`

--- README.md
+++ README.md
@@ -106,36 +106,6 @@ with `-statsd.add-suffix=false`.
 
 An example mapping configuration with `-statsd.add-suffix=false`:
 
-    # comments are allowed
-    test.dispatcher.*.*.*
-    name="dispatcher_events_total"
-    processor="$1"
-    action="$2"
-    outcome="$3"
-    job="test_dispatcher"
-
-    *.signup.*.*
-    name="signup_events_total"
-    provider="$2"
-    outcome="$3"
-    job="${1}_server"
-
-This would transform these example StatsD metrics into Prometheus metrics as
-follows:
-
-    test.dispatcher.FooProcessor.send.success
-     => dispatcher_events_total{processor="FooProcessor", action="send", outcome="success", job="test_dispatcher"}
-
-    foo_product.signup.facebook.failure
-     => signup_events_total{provider="facebook", outcome="failure", job="foo_product_server"}
-
-    test.web-server.foo.bar
-     => test_web__server_foo_bar{}
-
-
-YAML may also be used for the configuration if the passed filename ends in `.yml` or
-`.yaml`.  The above example mapping, in YAML, would be:
-
 ```yaml
 mappings:
 - match: test.dispatcher.*.*.*
@@ -153,7 +123,20 @@ mappings:
     job: "${1}_server"
 ```
 
-Using the YAML configuration, one may also set the timer type to "histogram". The 
+This would transform these example StatsD metrics into Prometheus metrics as
+follows:
+
+    test.dispatcher.FooProcessor.send.success
+     => dispatcher_events_total{processor="FooProcessor", action="send", outcome="success", job="test_dispatcher"}
+
+    foo_product.signup.facebook.failure
+     => signup_events_total{provider="facebook", outcome="failure", job="foo_product_server"}
+
+    test.web-server.foo.bar
+     => test_web__server_foo_bar{}
+
+
+In the configuration, one may also set the timer type to "histogram". The 
 default is "summary" as in the plain text configuration format.  For example,
 to set the timer type for a single metric:
 
--- mapper.go
+++ mapper.go
@@ -16,7 +16,6 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"path/filepath"
 	"regexp"
 	"strings"
 	"sync"
@@ -51,78 +50,6 @@ type metricMapping struct {
 	Labels    prometheus.Labels `yaml:"labels"`
 	TimerType timerType         `yaml:"timer_type"`
 	Buckets   []float64         `yaml:"buckets"`
-}
-
-type configLoadStates int
-
-const (
-	SEARCHING configLoadStates = iota
-	METRIC_DEFINITION
-)
-
-func (m *metricMapper) initFromString(fileContents string) error {
-	lines := strings.Split(fileContents, "\n")
-	numLines := len(lines)
-	state := SEARCHING
-
-	parsedMappings := []metricMapping{}
-	currentMapping := metricMapping{Labels: prometheus.Labels{}}
-	for i, line := range lines {
-		line = strings.TrimSpace(line)
-
-		// skip comments
-		if strings.HasPrefix(line, "#") {
-			continue
-		}
-
-		switch state {
-		case SEARCHING:
-			if line == "" {
-				continue
-			}
-			if !metricLineRE.MatchString(line) {
-				return fmt.Errorf("line %d: expected metric match line, got: %s", i, line)
-			}
-
-			// Translate the glob-style metric match line into a proper regex that we
-			// can use to match metrics later on.
-			metricRe := strings.Replace(line, ".", "\\.", -1)
-			metricRe = strings.Replace(metricRe, "*", "([^.]+)", -1)
-			currentMapping.regex = regexp.MustCompile("^" + metricRe + "$")
-
-			state = METRIC_DEFINITION
-
-		case METRIC_DEFINITION:
-			if (i == numLines-1) && (line != "") {
-				return fmt.Errorf("Line %d: missing terminating newline", i)
-			}
-			if line == "" {
-				if len(currentMapping.Labels) == 0 {
-					return fmt.Errorf("Line %d: metric mapping didn't set any labels", i)
-				}
-				if _, ok := currentMapping.Labels["name"]; !ok {
-					return fmt.Errorf("Line %d: metric mapping didn't set a metric name", i)
-				}
-				parsedMappings = append(parsedMappings, currentMapping)
-				state = SEARCHING
-				currentMapping = metricMapping{Labels: prometheus.Labels{}}
-				continue
-			}
-			if err := m.updateMapping(line, i, &currentMapping); err != nil {
-				return err
-			}
-		default:
-			panic("illegal state")
-		}
-	}
-
-	m.mutex.Lock()
-	defer m.mutex.Unlock()
-	m.Mappings = parsedMappings
-
-	mappingsCount.Set(float64(len(parsedMappings)))
-
-	return nil
 }
 
 func (m *metricMapper) initFromYAMLString(fileContents string) error {
@@ -189,12 +116,7 @@ func (m *metricMapper) initFromFile(fileName string) error {
 	if err != nil {
 		return err
 	}
-	switch strings.ToLower(filepath.Ext(fileName)) {
-	case ".yaml", ".yml":
-		return m.initFromYAMLString(string(mappingStr))
-	default:
-		return m.initFromString(string(mappingStr))
-	}
+	return m.initFromYAMLString(string(mappingStr))
 }
 
 func (m *metricMapper) getMapping(statsdMetric string) (*metricMapping, prometheus.Labels, bool) {
@@ -216,17 +138,4 @@ func (m *metricMapper) getMapping(statsdMetric string) (*metricMapping, promethe
 	}
 
 	return nil, nil, false
-}
-
-func (m *metricMapper) updateMapping(line string, i int, mapping *metricMapping) error {
-	matches := labelLineRE.FindStringSubmatch(line)
-	if len(matches) != 3 {
-		return fmt.Errorf("Line %d: expected label mapping line, got: %s", i, line)
-	}
-	label, value := matches[1], matches[2]
-	if label == "name" && !metricNameRE.MatchString(value) {
-		return fmt.Errorf("Line %d: metric name '%s' doesn't match regex '%s'", i, value, metricNameRE)
-	}
-	(*mapping).Labels[label] = value
-	return nil
 }
